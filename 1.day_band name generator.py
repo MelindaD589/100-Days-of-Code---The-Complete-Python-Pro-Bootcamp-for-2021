3f04d64f39d749383a334fe0c7219034427f4c66
Melinda Dothan <68985823+MelindaD589@users.noreply.github.com>
2021-08-13 22:51:43 +0200

    Keeping only the project

---
+++ 1.day_band name generator.py
@@ -0,0 +1,17 @@
+# PROJECT: to create a band name generator
+
+#1. Create a greeting for your program.
+print("Hello! How are you today?")
+#2. Ask the user for the city that they grew up in.
+city = input("Where did you grow up?\n")
+#3. Ask the user for the name of a pet.
+pet = input("What was the name of your first pet?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("Your band name could be " + city + " " + pet)
+
+# Hello! How are you today?
+# Where did you grow up?
+# Debrecen
+# What was the name of your first pet?
+# Swan
+# Your band name could be Debrecen Swan
